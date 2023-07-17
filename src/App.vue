249ed0b969261ed21b6119d3874e82e6478a4232
神奇のDz <miku@ecy.pink>
2023-07-18 03:28:00 +0800

    更新根组件

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,33 @@
     <Miku />
     <div class="app">
         <h1>EcyPink</h1>
+        <Nav :navs="navs" @changed="changePage" />
         <GG>
             <hitokoto />
         </GG>
-        <Links :links="links" />
+        <KeepAlive>
+            <component :is="currentActivePage" :links="links"></component>
+        </KeepAlive>
         <Footer text="www.miku.show" icp="萌ICP备20233993号"></Footer>
     </div>
 </template>
 
 <script setup>
-import { defineAsyncComponent } from 'vue';
+import { defineAsyncComponent, shallowRef } from 'vue';
 const GG = defineAsyncComponent(() => import('./components/GG.vue'))
 const Miku = defineAsyncComponent(() => import('./components/Miku.vue'));
 const Links = defineAsyncComponent(() => import('./views/Links.vue'));
+const Setu = defineAsyncComponent(() => import('./views/Setu.vue'));
 const hitokoto = defineAsyncComponent(() => import('./components/Hitokoto.vue'));
 const Footer = defineAsyncComponent(() => import('./components/Footer.vue'));
+const Nav = defineAsyncComponent(() => import('./components/Nav.vue'));
+import navs from './data/navs'
 import links from './data/links'
+const pages = [Links, Setu];
+const currentActivePage = shallowRef(pages[0]);
+function changePage(index) {
+    currentActivePage.value = pages[index];
+}
 </script>
 
 <style  scoped>
