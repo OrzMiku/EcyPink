d7a6b2b403d7cf65ddaa610760957d5ffa0cf076
神奇のDz <miku@ecy.pink>
2023-07-18 02:07:31 +0800

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
+    <Miku />
     <div class="app">
-        <Miku />
-        <h1>友情链接</h1>
-        <GG>君埋泉下泥销骨，我寄人间雪满头。</GG>
-        <template v-for="item in links" :key="item">
-            <LinkCard :title="item.title" :description="item.description" :url="item.url" />
-        </template>
+        <h1>EcyPink</h1>
+        <GG>
+            <hitokoto />
+        </GG>
+        <Links :links="links" />
+        <Footer text="www.miku.show" icp="萌ICP备20233993号"></Footer>
     </div>
 </template>
 
 <script setup>
 import { defineAsyncComponent } from 'vue';
-const LinkCard = defineAsyncComponent(() => import('./components/LinkCard.vue'))
 const GG = defineAsyncComponent(() => import('./components/GG.vue'))
 const Miku = defineAsyncComponent(() => import('./components/Miku.vue'));
+const Links = defineAsyncComponent(() => import('./views/Links.vue'));
+const hitokoto = defineAsyncComponent(() => import('./components/Hitokoto.vue'));
+const Footer = defineAsyncComponent(() => import('./components/Footer.vue'));
 import links from './data/links'
 </script>
 
