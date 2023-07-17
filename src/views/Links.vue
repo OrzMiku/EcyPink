78c0acdd945ee8f397e34ab14f61994c7a0b6744
神奇のDz <miku@ecy.pink>
2023-07-18 02:07:12 +0800

    将友情链接分离出根组件

---
+++ src/views/Links.vue
@@ -0,0 +1,17 @@
+<template>
+    <h2>友情链接</h2>
+    <template v-for="item in links" :key="item">
+        <LinkCard :title="item.title" :description="item.description" :url="item.url" />
+    </template>
+</template>
+
+<script setup>
+import { defineAsyncComponent } from 'vue';
+const LinkCard = defineAsyncComponent(() => import('../components/LinkCard.vue'))
+const props = defineProps({
+    links: {
+        type: Object,
+        default: () => { }
+    }
+})
+</script>
