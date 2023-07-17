890a7ba3d5a91a93de29f7a7622ac7768cf032a3
神奇のDz <miku@ecy.pink>
2023-07-18 03:28:21 +0800

    bug fix

--- src/utils/hitokoto.js
+++ src/utils/hitokoto.js
@@ -1,7 +1,6 @@
 
 export function getHitokoto() {
     return new Promise(resolve => {
-        let res = "";
         let xhr = new XMLHttpRequest();
         xhr.open('GET', 'https://v1.hitokoto.cn/', true);
         xhr.send();
