77838ef22a68f74e328b59fdad3727280b3d3a19
神奇のDz <miku@ecy.pink>
2023-07-18 02:05:33 +0800

    添加Footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="footer">
+        <template v-if="hasText">{{ text }}</template>
+        <template v-else>配置错误</template>
+        <template v-if="hasIcp"> | {{ icp }}</template>
+    </div>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+const props = defineProps({
+    icp: {
+        type: String,
+        default: "",
+    },
+    text: {
+        type: String,
+        default: ""
+    }
+});
+
+const hasText = computed(() => {
+    return props.text === "" ? false : true;
+})
+const hasIcp = computed(() => {
+    return props.icp === "" ? false : true;
+})
+</script>
+
+<style scoped>
+.footer {
+    text-align: center;
+    color: #606060;
+    font-weight: bolder;
+}
+</style>
