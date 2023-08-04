167d38aed6bbb627a65dbc1ebe65407f8348ed9d
神奇のDz <miku@ecy.pink>
2023-08-05 06:08:39 +0800

    新增页面标题更新

--- src/main.js
+++ src/main.js
@@ -2,6 +2,17 @@ import { createApp } from 'vue'
 import router from './router'
 import App from './App.vue'
 
+const site = {
+    title: "EcyPink"
+}
+
+router.beforeEach((to, from, next) => {
+    if (to.meta.title) {
+        document.title = site['title'] + " - " + to.meta.title
+    }
+    next()
+})
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
