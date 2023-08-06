b03fa1842298a8f971a68d068fb73ad3846c6d5e
OrzMiku <miku@ecy.pink>
2023-08-06 23:39:02 +0800

    删除公告

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,6 @@
         <GG>
             <hitokoto />
         </GG>
-        <GG>
-            <p>
-                关注 <a href="https://space.bilibili.com/517141742">BiliBili@尹阿泡Apo</a> 喵。
-            </p>
-            <p>
-                关注 <a href="https://space.bilibili.com/517141742">BiliBili@尹阿泡Apo</a> 谢谢喵。
-            </p>
-            一起玩剑三加QQ群748860669。
-        </GG>
         <RouterView></RouterView>
         <Footer text="www.miku.show" icp="萌ICP备20233993号"></Footer>
     </div>
