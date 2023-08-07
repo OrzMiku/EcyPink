d909528bf34aca61e7eec94c49f040f554d2e478
OrzMiku <miku@ecy.pink>
2023-08-07 18:28:00 +0800

    优化参数获取函数，R18判断函数

--- src/utils/params.js
+++ src/utils/params.js
@@ -1,25 +1,11 @@
+import { useRoute } from 'vue-router'
+
 export function getParams() {
-    const res = {}
-    let raw = window.location.href;
-    raw = raw.split('?')[1];
-    if (raw) {
-        raw = raw.split('&');
-        for (let i = 0; i < raw.length; i++) {
-            let key = raw[i].split('=')[0];
-            let value = raw[i].split('=')[1];
-            res[key] = value;
-        }
-    }
-    return res;
+    const route = useRoute()
+    return route.query
 }
 
 export function isR18() {
     const params = getParams();
-    if ('r18' in params) {
-        if (params['r18'] !== 'true' && params['r18'] !== '1') {
-            return false;
-        }
-        return true;
-    }
-    return false;
+    return parseInt(params['r18']) ? true : false;
 }
