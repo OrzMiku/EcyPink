52033f73fc9d5f49b33551df5d6e3e3401a7e59d
OrzMiku <miku@ecy.pink>
2023-08-07 18:24:45 +0800

    axios简单封装

---
+++ src/service/mikureq.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+
+class mikureq {
+    constructor(baseURL, timeout = 10000) {
+        this.instance = axios.create({
+            baseURL, timeout
+        })
+    }
+
+    req(config) {
+        return new Promise((resolve, reject) => {
+            this.instance.request(config)
+                .then(res => {
+                    resolve(res.data)
+                })
+                .catch(err => {
+                    reject(err)
+                })
+        })
+    }
+
+    get(config) {
+        return this.req({ ...config, method: "get" })
+    }
+    post(config) {
+        return this.req({ ...config, method: "post" })
+    }
+}
+
+export default mikureq
